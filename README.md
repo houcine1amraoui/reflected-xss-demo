b21a82a16e90354ad903e526fd511d205de28fc7
houcine1amraoui <houcine1amraoui@gmail.com>
2024-03-02 17:33:35 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # reflected-xss-demo
+
+# An example of an XSS attack
+
+The server exposes "/hello" endpoint expects a name passed as a query parameter
+and returns message saying: "Hello, $name".
+
+Reflected XSS:
+
+Since the server is concatenating the user input to the output message without proper validation (e.g., escaping it), this makes it vulnerable to Reflected XSS.
+In XSS attacks, injected scripts reflect off to web server and execute immediately.
+
+## Example:
+
+Attacker can crafts the following URL and sends it to the victim
+http://localhost:2000/hello?name=%3Cscript%3Ealert(1)%3C/script%3E
+
+The script is then reflected to the browser that runs it immediatly, leading to accessing the cookies of HttpOnly attribute is not set to true.
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// The /hello endpoint generates page output in code.
+// It expects a name as a parameter to say "Hello, $name"
+// and concatenates the user input to the output without escaping it.
+
+// However this endpoint is vulnerable to Reflected XSS
+// Reflected XSS: Injected scripts reflect off a web server and execute immediately,
+// Occurs since user input is accepted without proper validation.
+// Example: Attacker can crafts the following URL and sends it to the victim
+// http://localhost:2000/hello?name=%3Cscript%3Ealert(1)%3C/script%3E
+app.get("/hello", function (req, res) {
+  res.send(`Hello, ${req.query.name}`);
+});
+
+const PORT = 1000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
